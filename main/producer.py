c2b9cc55a96a02e18de629100316e2ba052f7f08
guirat <guirat.meds@gmail.com>
2021-05-31 07:38:25 +0200

    add main app

---
+++ main/producer.py
@@ -0,0 +1,12 @@
+import pika, json
+
+params = pika.URLParameters('amqps://mxudpzkw:c53WI0AwW4kRJ_WDx3oZi2VOMVQnNBnJ@rat.rmq2.cloudamqp.com/mxudpzkw')
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='admin', body=json.dumps(body), properties=properties)
